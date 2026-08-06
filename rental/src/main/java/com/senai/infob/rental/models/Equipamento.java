4ed892cea5783cd77a968839e0082120f8822927
DaviMZ08 <xxdavioficial@gmail.com>
2026-08-06 11:48:51 -0300

    comi

--- rental/src/main/java/com/senai/infob/rental/models/Equipamento.java
+++ rental/src/main/java/com/senai/infob/rental/models/Equipamento.java
@@ -1,5 +1,109 @@
 package com.senai.infob.rental.models;
 
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "equipamento")
 public class Equipamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_equipamento")
+    private Integer idEquipamento;
+    
+    @Column(name="nome")
+    private String nome;
+
+    @Column(name="marca")
+    private String marca;
+
+    @Column(name="modelo")
+    private String modelo;
+
+    @Column(name="descricao")
+    private String descricao;
+
+    @Column(name="estoque")
+    private Integer estoque;
+
+    @OneToOne(mappedBy = "equipamento")
+    private Categoria categoria;
+
+    public Equipamento() {
+    }
+
+    public Equipamento(Integer idEquipamento, String nome, String marca, String modelo, String descricao, Integer estoque, Categoria categoria) {
+        this.idEquipamento = idEquipamento;
+        this.nome = nome;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.descricao = descricao;
+        this.estoque = estoque;
+        this.categoria = categoria;
+    }
+
+    public Integer getIdEquipamento() {
+        return idEquipamento;
+    }
+
+    public void setIdEquipamento(Integer idEquipamento) {
+        this.idEquipamento = idEquipamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
     
 }
