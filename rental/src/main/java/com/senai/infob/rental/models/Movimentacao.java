2c3076daeda1cc0b1ea829bbef289ffe086a5e5c
DaviMZ08 <xxdavioficial@gmail.com>
2026-08-06 11:49:40 -0300

    commit

--- rental/src/main/java/com/senai/infob/rental/models/Movimentacao.java
+++ rental/src/main/java/com/senai/infob/rental/models/Movimentacao.java
@@ -1,5 +1,19 @@
 package com.senai.infob.rental.models;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="movimentacao")
 public class Movimentacao {
-    
+    @Id
+    @Column(name="id_movimentacao")
+    private Integer idMovimentacao;
+
+    @Column(name="data_movimentacao")
+    private Timestamp dataMovimentacao;
 }
